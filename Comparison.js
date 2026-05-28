403241202950fafd98c2479af1b8b9d0bd2f1479
kausiksarkar10 <kausiksarkar.ix.c.29.2020@gmail.com>
2026-05-28 10:53:27 +0530

    Add files via upload
    Basic knowledge about comparison operators in javascript

---
+++ Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);//returns false because the types are different
+console.log("2" == 2);//returns true because the string "2" is converted to the number 2 before comparison
